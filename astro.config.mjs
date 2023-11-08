b6e64bc9b782eefed9b63420f74f0b052def030b
Nil <nilinking@gmail.com>
2023-11-08 20:10:21 +0100

    Finished Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://neon-marzipan-6bbabd.netlify.app/",
+  integrations: [preact()]
+});
